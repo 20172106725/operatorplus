d87c08f2ac8ba9e122d2b45a15873688a21a715e
20172106725 <15134008212@163.com>
2018-05-28 17:42:14 +0800

    运算符重载(减法)
    未完成

--- operatorplus/operatorplus.cpp
+++ operatorplus/operatorplus.cpp
@@ -23,25 +23,64 @@ void CFeet::setvalue(int ft, int in)
 void CFeet::display()
 {
 	cout << feet << "Ó¢³ß" << inches << "Ó¢´ç" << endl;
-}
-CFeet CFeet::add(CFeet & objf)
+};
+/*CFeet CFeet::add(CFeet & objf)
 {
-	CFeet temp;
-	temp.setvalue(feet - objf.feet, inches - objf.inches);
-	return temp;
-}
+	if (inches > objf.inches)
+	{
+		CFeet temp;
+		temp.setvalue(feet - objf.feet, inches - objf.inches);
+		return temp;
+	}
+	else
+	{
+		CFeet temp;
+		temp.setvalue(feet - objf.feet, objf.inches - inches);
+		return temp;
+	}
+}*/
 CFeet CFeet :: operator - (CFeet & objf)
 {
-	CFeet temp;
-	temp.setvalue(feet - objf.feet, inches - objf.inches);
-	return temp;
-}
+	if (feet > objf.feet)
+	{
+		if (inches > objf.inches)
+		{
+			CFeet temp;
+			temp.setvalue(feet - objf.feet, inches - objf.inches);
+			return temp;
+		}
+		else
+		{
+			CFeet temp;
+			temp.setvalue(feet - objf.feet, objf.inches-inches);
+			return temp;
 
+		}
+	}
+	else
+	{
+		inches = inches + (feet * 12);
+		objf.inches = objf.inches + (objf.feet * 12);
+		if (inches > objf.inches)
+		{
+			CFeet temp;
+			temp.setvalue(0, inches - objf.inches);
+			return temp;
+		}
+		else
+		{
+			inches = inches + (feet * 12);
+			CFeet temp;
+			temp.setvalue(0, objf.inches - inches);
+			return temp;
+		}
+	}
+}
 int main()
 {
 	CFeet A, B, C;
-	A.setvalue(10, 19);
-	B.setvalue(9, 8);
+	A.setvalue(1, 3);
+	B.setvalue(2, 2);
 	C = A - B;
 	C.display();
 	return 0;
