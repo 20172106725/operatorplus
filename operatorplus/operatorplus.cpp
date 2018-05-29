30669c5f4b1ffbef647363aa3d54414304e2a935
20172106725 <15134008212@163.com>
2018-05-29 15:40:49 +0800

    英尺英寸相减(还没完成)
    借鉴而来,该了一点点

--- operatorplus/operatorplus.cpp
+++ operatorplus/operatorplus.cpp
@@ -12,76 +12,53 @@ private:
 public:
 	void setvalue(int ft, int in);
 	void display();
-	CFeet add(CFeet & objf);
-	CFeet operator -(CFeet & objf);
+	//CFeet add(CFeet & objf);
+	CFeet operator+(CFeet & objf);
 };
 void CFeet::setvalue(int ft, int in)
 {
-	feet = ft + in / 12;
-	inches = in % 12;
+	feet = ft+in/12;
+	inches = in%12;
 }
 void CFeet::display()
 {
 	cout << feet << "英尺" << inches << "英寸" << endl;
-};
-/*CFeet CFeet::add(CFeet & objf)
+}
+/*{
+	if (inches < 0)
+	{
+		cout << inches / 12 << "英尺" << -inches % 12 << "英寸" << endl;
+	}
+	if (inches > 0)
+	{
+		cout << inches / 12 << "英尺" << inches % 12 << "英寸" << endl;
+	}
+}
+/*CFeet CFeet::add(CFeet & objf)  //objf是对象引用b
 {
-	if (inches > objf.inches)
-	{
-		CFeet temp;
-		temp.setvalue(feet - objf.feet, inches - objf.inches);
-		return temp;
-	}
-	else
-	{
-		CFeet temp;
-		temp.setvalue(feet - objf.feet, objf.inches - inches);
-		return temp;
-	}
+CFeet temp;
+temp.setvalue(feet + objf.feet, inches + objf.inches);
+return temp;
 }*/
-CFeet CFeet :: operator - (CFeet & objf)
+CFeet CFeet::operator+(CFeet & objf)
 {
-	if (feet > objf.feet)
+	CFeet temp;
+	if (feet > 0)
 	{
-		if (inches > objf.inches)
-		{
-			CFeet temp;
-			temp.setvalue(feet - objf.feet, inches - objf.inches);
-			return temp;
-		}
-		else
-		{
-			CFeet temp;
-			temp.setvalue(feet - objf.feet, objf.inches-inches);
-			return temp;
-
-		}
+		temp.setvalue(0, inches = (feet * 12 + inches) - (objf.feet * 12 + objf.inches));
 	}
 	else
 	{
-		inches = inches + (feet * 12);
-		objf.inches = objf.inches + (objf.feet * 12);
-		if (inches > objf.inches)
-		{
-			CFeet temp;
-			temp.setvalue(0, inches - objf.inches);
-			return temp;
-		}
-		else
-		{
-			inches = inches + (feet * 12);
-			CFeet temp;
-			temp.setvalue(0, objf.inches - inches);
-			return temp;
-		}
+		temp.setvalue(0, inches = (feet * 12 - inches) - (objf.feet * 12 + objf.inches));
 	}
+	return temp;
 }
 int main()
 {
 	CFeet A, B, C;
-	A.setvalue(1, 3);
-	B.setvalue(2, 2);
-	C = A - B;
+	A.setvalue(-2, 3);
+	B.setvalue(2, 3);
+	C = A + B; //有CFeet CFeet::operator+(CFeet & objf)的时候能用c=a+b ，有CFeet CFeet::add(CFeet & objf)时用c=a.add(b)
 	C.display();
 	return 0;
 }
